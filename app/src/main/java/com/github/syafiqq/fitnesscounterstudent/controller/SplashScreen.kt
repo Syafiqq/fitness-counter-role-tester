bc62f121e4fc6166af307a038f59d164df6e7c7f
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-11-05 12:44:11 +0700

    Add Splash screen Activity

---
+++ app/src/main/java/com/github/syafiqq/fitnesscounterstudent/controller/SplashScreen.kt
@@ -0,0 +1,80 @@
+package com.github.syafiqq.fitnesscounterstudent.controller
+
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import com.github.syafiqq.fitnesscounterstudent.R
+import timber.log.Timber
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+class SplashScreen: AppCompatActivity()
+{
+    private val hideHandler = Handler()
+    private val hideOperation = Runnable { hide() }
+    private var mContentView: View? = null
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        Timber.d("onCreate")
+
+        super.onCreate(savedInstanceState)
+
+        super.setContentView(R.layout.activity_splash_screen)
+
+        this.mContentView = findViewById(R.id.fullscreen_content)
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?)
+    {
+        Timber.d("onPostCreate")
+
+        super.onPostCreate(savedInstanceState)
+
+        // Trigger the initial hide() shortly after the activity has been
+        // created, to briefly hint to the user that UI controls
+        // are available.
+        this.delayedHide(50)
+    }
+
+    override fun onPostResume()
+    {
+        Timber.d("onPostResume")
+
+        super.onPostResume()
+    }
+
+    override fun onDestroy()
+    {
+        Timber.d("onDestroy")
+
+        super.onDestroy()
+    }
+
+    private fun hide()
+    {
+        Timber.d("hide")
+
+        // Hide UI first
+        val actionBar = supportActionBar
+        actionBar?.hide()
+
+        // Schedule a runnable to remove the status and navigation bar after a delay
+        //this.hideHandler.postDelayed(this.hideCompletelyOperation, UI_ANIMATION_DELAY);
+    }
+
+    /**
+     * Schedules a call to hide() in [300] milliseconds, canceling any
+     * previously scheduled calls.
+     */
+    private fun delayedHide(delayMillis: Int)
+    {
+        Timber.d("hideCompletely")
+
+        this.hideHandler.removeCallbacks(this.hideOperation)
+        this.hideHandler.postDelayed(this.hideOperation, delayMillis.toLong())
+    }
+}
