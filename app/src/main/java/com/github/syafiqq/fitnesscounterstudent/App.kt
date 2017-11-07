a3c350ad154acbba85825dbf14b6a8112498a398
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-11-07 13:31:30 +0700

    Reformat code

--- app/src/main/java/com/github/syafiqq/fitnesscounterstudent/App.kt
+++ app/src/main/java/com/github/syafiqq/fitnesscounterstudent/App.kt
@@ -5,6 +5,7 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.os.Build
+import org.acra.ACRA
 import org.acra.ReportField
 import org.acra.ReportingInteractionMode
 import org.acra.annotation.ReportsCrashes
@@ -22,30 +23,38 @@ import timber.log.Timber
         customReportContent = arrayOf(ReportField.APP_VERSION_CODE, ReportField.APP_VERSION_NAME, ReportField.ANDROID_VERSION, ReportField.PHONE_MODEL, ReportField.CUSTOM_DATA, ReportField.STACK_TRACE, ReportField.LOGCAT),
         mode = ReportingInteractionMode.TOAST,
         resToastText = R.string.crash_toast_text)
-class App : Application() {
-
-    override fun onCreate() {
+class App: Application()
+{
+    override fun onCreate()
+    {
         super.onCreate()
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
             val channelId = getString(R.string.default_notification_channel_id)
             val channelName = getString(R.string.default_notification_channel_name)
-            val notificationManager = getSystemService(NotificationManager::class.java)
-            notificationManager?.createNotificationChannel(NotificationChannel(channelId,
-                    channelName, NotificationManager.IMPORTANCE_LOW))
+            getSystemService(NotificationManager::class.java).apply {
+                this?.createNotificationChannel(NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_LOW))
+            }
         }
     }
 
-    override fun attachBaseContext(base: Context) {
+    override fun attachBaseContext(base: Context)
+    {
         super.attachBaseContext(base)
         this.initializeTimber()
-        //ACRA.init(this)
+        if (!BuildConfig.DEBUG) ACRA.init(this)
     }
 
-    private fun initializeTimber() {
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
-        }
+    private fun initializeTimber()
+    {
+        Timber.plant(
+                if (BuildConfig.DEBUG)
+                    Timber.DebugTree()
+                else
+                    Timber.DebugTree()
+        )
+
         Timber.d("Timber initialized")
     }
 }
