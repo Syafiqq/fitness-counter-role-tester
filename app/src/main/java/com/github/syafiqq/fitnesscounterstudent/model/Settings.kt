3522258c50689327fac0814192653aa66dc6b110
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-11-13 06:23:02 +0700

    Add settings object

---
+++ app/src/main/java/com/github/syafiqq/fitnesscounterstudent/model/Settings.kt
@@ -0,0 +1,44 @@
+package com.github.syafiqq.fitnesscounterstudent.model
+
+import com.github.syafiqq.fitnesscounterstudent.model.orm.Groups
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import timber.log.Timber
+
+/**
+ * This fitness-counter-student project created by :
+ * Name         : syafiq
+ * Date / Time  : 12 November 2017, 9:53 PM.
+ * Email        : id.muhammad.syafiq@gmail.com
+ * Github       : Syafiqq
+ */
+
+object Settings
+{
+    fun defaultGroup(success: (Groups?) -> Unit, error: (DatabaseError?) -> Unit = { Timber.d(it?.message) })
+    {
+        val ref = FirebaseDatabase.getInstance().getReference("groups")
+        ref.addListenerForSingleValueEvent(object: ValueEventListener
+        {
+            override fun onCancelled(dbError: DatabaseError?)
+            {
+                Timber.d("onCancelled")
+
+                error(dbError)
+            }
+
+            override fun onDataChange(snapshot: DataSnapshot?)
+            {
+                Timber.d("onDataChange")
+
+                val group = snapshot
+                        ?.children
+                        ?.map { it.getValue(Groups::class.java) }
+                        ?.firstOrNull { it?.name == "student" }
+                success(group)
+            }
+        })
+    }
+}
