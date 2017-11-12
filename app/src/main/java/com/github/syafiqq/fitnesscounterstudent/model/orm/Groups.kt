94769c78a1f831d04d69d4df24139b4e5ef0468e
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-11-13 06:16:15 +0700

    Add groups data class

---
+++ app/src/main/java/com/github/syafiqq/fitnesscounterstudent/model/orm/Groups.kt
@@ -0,0 +1,10 @@
+package com.github.syafiqq.fitnesscounterstudent.model.orm
+
+/**
+ * This fitness-counter-student project created by :
+ * Name         : syafiq
+ * Date / Time  : 12 November 2017, 7:36 PM.
+ * Email        : id.muhammad.syafiq@gmail.com
+ * Github       : Syafiqq
+ */
+data class Groups(var id: Int? = null, var name: String? = null)
