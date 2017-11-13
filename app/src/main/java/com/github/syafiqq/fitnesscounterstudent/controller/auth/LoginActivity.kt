f3028356ef3777d13745170dbaf8b23811b593ef
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-11-13 17:59:00 +0700

    Fix login flow

--- app/src/main/java/com/github/syafiqq/fitnesscounterstudent/controller/auth/LoginActivity.kt
+++ app/src/main/java/com/github/syafiqq/fitnesscounterstudent/controller/auth/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.github.syafiqq.fitnesscounterstudent.controller.auth
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.text.TextUtils
 import android.view.KeyEvent
@@ -12,6 +13,10 @@ import android.widget.TextView
 import android.widget.Toast
 import com.afollestad.materialdialogs.MaterialDialog
 import com.github.syafiqq.fitnesscounterstudent.R
+import com.github.syafiqq.fitnesscounterstudent.custom.com.google.firebase.database.DefaultErrorValueEventListener
+import com.github.syafiqq.fitnesscounterstudent.model.Settings
+import com.github.syafiqq.fitnesscounterstudent.model.firebase.Path
+import com.github.syafiqq.fitnesscounterstudent.model.orm.Groups
 import com.google.android.gms.tasks.Task
 import com.google.firebase.FirebaseNetworkException
 import com.google.firebase.auth.AuthResult
@@ -20,6 +25,10 @@ import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 import com.google.firebase.auth.FirebaseAuthInvalidUserException
 import com.google.firebase.auth.FirebaseAuthUserCollisionException
 import com.google.firebase.auth.FirebaseAuthWeakPasswordException
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_login.*
 import timber.log.Timber
 
@@ -158,27 +167,76 @@ class LoginActivity: AppCompatActivity()
     {
         Timber.d("onAuthComplete")
 
-        this.dialog.dismiss()
+        fun grantTo(group: Groups, user: FirebaseUser)
+        {
+            Timber.d("grantTo")
+
+            this@LoginActivity.dialog.dismiss()
+
+            Snackbar.make(this.constraintlayout_root, R.string.request_privilege, Snackbar.LENGTH_INDEFINITE)
+                    .setAction(R.string.yes) {
+                        this@LoginActivity.dialog.show()
+                        Auth.grantTo(group, user, DatabaseReference.CompletionListener { error, _ ->
+                            error?.let { grantTo(group, user) } ?: onAuthComplete(result)
+                        })
+                    }
+                    .show()
+        }
+
+        fun checkPrivilege(user: FirebaseUser)
+        {
+            fun resolveNetwork()
+            {
+                Toast.makeText(this@LoginActivity, R.string.label_auth_network_issue, Toast.LENGTH_SHORT).show()
+                checkPrivilege(user)
+            }
+
+            Settings.defaultGroup(
+                    {
+                        it?.let {
+                            FirebaseDatabase.getInstance().getReference("/${Path.USERS_GROUPS}/${user.uid}/${it.id}").apply {
+                                this.addListenerForSingleValueEvent(object: DefaultErrorValueEventListener
+                                {
+                                    override fun onDataChange(snapshot: DataSnapshot?)
+                                    {
+                                        Timber.d("onDataChange")
+                                        if (snapshot?.exists()!!)
+                                        {
+                                            this@LoginActivity.dialog.dismiss()
+                                            Timber.d("Login Success")
+                                        }
+                                        else
+                                        {
+                                            Toast.makeText(this@LoginActivity, R.string.auth_no_previlege_access, Toast.LENGTH_SHORT).show()
+                                            grantTo(it, user)
+                                        }
+                                    }
+                                })
+                            }
+                        } ?: resolveNetwork()
+                    }, { resolveNetwork() })
+        }
+
         if (result.isSuccessful)
         {
-            Timber.d("Success Login")
+            FirebaseAuth.getInstance().currentUser?.let { checkPrivilege(it) }
         }
         else
         {
+            this.dialog.dismiss()
+
             Timber.d("Failed Login")
 
             Timber.e(result.exception)
-            Toast.makeText(this, super.getResources().getString(
-                    when (result.exception!!)
-                    {
-                        is FirebaseAuthInvalidUserException        -> R.string.label_auth_email_not_exists
-                        is FirebaseAuthWeakPasswordException       -> R.string.label_auth_weak_password
-                        is FirebaseAuthInvalidCredentialsException -> R.string.label_auth_invalid_credential
-                        is FirebaseAuthUserCollisionException      -> R.string.label_auth_email_exists
-                        is FirebaseNetworkException                -> R.string.label_auth_network_issue
-                        else                                       -> R.string.label_login_failed
-                    }
-            ), Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, when (result.exception!!)
+            {
+                is FirebaseAuthInvalidUserException        -> R.string.label_auth_email_not_exists
+                is FirebaseAuthWeakPasswordException       -> R.string.label_auth_weak_password
+                is FirebaseAuthInvalidCredentialsException -> R.string.label_auth_invalid_credential
+                is FirebaseAuthUserCollisionException      -> R.string.label_auth_email_exists
+                is FirebaseNetworkException                -> R.string.label_auth_network_issue
+                else                                       -> R.string.label_login_failed
+            }, Toast.LENGTH_SHORT).show()
         }
     }
 
