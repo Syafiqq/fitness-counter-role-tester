52181d456a1d6e575913af7f33e1cf667f5b288e
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-01-08 20:55:17 +0700

    Remove Unnecessary code Add Group Name

--- app/src/main/java/com/github/syafiqq/fitnesscounter/role/student/model/Settings.kt
+++ app/src/main/java/com/github/syafiqq/fitnesscounter/role/student/model/Settings.kt
@@ -1,12 +1,5 @@
 package com.github.syafiqq.fitnesscounter.role.student.model
 
-import com.github.syafiqq.fitnesscounter.role.student.model.orm.Groups
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
-import timber.log.Timber
-
 /**
  * This fitness-counter-student project created by :
  * Name         : syafiq
@@ -17,30 +10,5 @@ import timber.log.Timber
 
 object Settings
 {
-    fun defaultGroup(success: (Groups?) -> Unit, error: (DatabaseError?) -> Unit = { Timber.d(it?.message) })
-    {
-        val ref = FirebaseDatabase.getInstance().getReference("groups")
-        ref.addListenerForSingleValueEvent(object: ValueEventListener
-        {
-            override fun onCancelled(dbError: DatabaseError?)
-            {
-                Timber.d("onCancelled")
-
-                error(dbError)
-            }
-
-            override fun onDataChange(snapshot: DataSnapshot?)
-            {
-                Timber.d("onDataChange")
-
-                val group = snapshot
-                        ?.children
-                        ?.map { it.getValue(Groups::class.java) }
-                        ?.firstOrNull { it?.name == "student" }
-                success(group)
-            }
-        })
-    }
-
     val GROUP_NAME = "student"
 }
