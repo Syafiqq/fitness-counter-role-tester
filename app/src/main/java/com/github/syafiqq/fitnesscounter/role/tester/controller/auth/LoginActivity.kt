40d49431b981f6eca520927ceee66275f958e2b1
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-02-21 21:23:38 +0700

    - remove -- remove recover button

--- app/src/main/java/com/github/syafiqq/fitnesscounter/role/tester/controller/auth/LoginActivity.kt
+++ app/src/main/java/com/github/syafiqq/fitnesscounter/role/tester/controller/auth/LoginActivity.kt
@@ -51,7 +51,6 @@ class LoginActivity: AppCompatActivity()
         this.edittext_password.setOnEditorActionListener(this::onEditorActionClicked)
         this.button_sumbit.setOnClickListener(this::onSubmitButtonClicked)
         this.button_register.setOnClickListener(this::onRegisterButtonClicked)
-        this.button_recover.setOnClickListener { super.startActivity(Intent(this, PasswordResetActivity::class.java)) }
     }
 
     override fun onDestroy()
