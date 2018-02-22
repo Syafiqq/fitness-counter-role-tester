554ae74a1c55f5c93e146f0490efc22a2e26f922
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-02-23 06:18:10 +0700

    Add dashboard Drawer Resolver
    (cherry picked from commit e6f65aa)

--- app/src/main/java/com/github/syafiqq/fitnesscounter/role/tester/util/resolver/MenuResolver.kt
+++ app/src/main/java/com/github/syafiqq/fitnesscounter/role/tester/util/resolver/MenuResolver.kt
@@ -1,5 +1,7 @@
 package com.github.syafiqq.fitnesscounter.role.tester.util.resolver
 
+import com.github.syafiqq.fitnesscounter.role.tester.R
+
 /**
  * This fitness-counter-role-tester project created by :
  * Name         : syafiq
@@ -8,3 +10,16 @@ package com.github.syafiqq.fitnesscounter.role.tester.util.resolver
  * Github       : Syafiqq
  */
 object MenuResolver
+{
+    val dashboardDrawerResolver = mutableMapOf(
+            // @formatter:off
+            "Medical Check"  to R.id.nav_health,
+            "Ulinois"        to R.id.nav_ulinois,
+            "Vertical Jump"  to R.id.nav_vertical_jump,
+            "Throwing Ball"  to R.id.nav_throwing_ball,
+            "Push Up"        to R.id.nav_push_up,
+            "Sit Up"         to R.id.nav_sit_up,
+            "Run 1600 m"     to R.id.nav_run_1600m
+            // @formatter:on
+    )
+}
