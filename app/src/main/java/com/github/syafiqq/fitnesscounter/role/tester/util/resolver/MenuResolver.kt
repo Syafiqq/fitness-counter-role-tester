b6e5f5ee64925a083007d32c84f4eeb9e0892c3a
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-02-23 06:17:53 +0700

    Add Menu Resolver
    (cherry picked from commit ac2a33f)

---
+++ app/src/main/java/com/github/syafiqq/fitnesscounter/role/tester/util/resolver/MenuResolver.kt
@@ -0,0 +1,10 @@
+package com.github.syafiqq.fitnesscounter.role.tester.util.resolver
+
+/**
+ * This fitness-counter-role-tester project created by :
+ * Name         : syafiq
+ * Date / Time  : 23 February 2018, 6:12 AM.
+ * Email        : id.muhammad.syafiq@gmail.com
+ * Github       : Syafiqq
+ */
+object MenuResolver
