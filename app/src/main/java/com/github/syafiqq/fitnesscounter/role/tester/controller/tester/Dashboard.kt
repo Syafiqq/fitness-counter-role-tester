14300ec22b18580719df324d6c694735c825a58c
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-02-24 08:27:07 +0700

    Add drawer

--- app/src/main/java/com/github/syafiqq/fitnesscounter/role/tester/controller/tester/Dashboard.kt
+++ app/src/main/java/com/github/syafiqq/fitnesscounter/role/tester/controller/tester/Dashboard.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import com.github.syafiqq.fitnesscounter.role.tester.R
-
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
 import kotlinx.android.synthetic.main.tester_activity_dashboard.*
 
 class Dashboard: AppCompatActivity()
 {
+
+    private lateinit var drawer: Drawer
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
@@ -20,6 +23,13 @@ class Dashboard: AppCompatActivity()
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+        this.drawer = DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withTranslucentStatusBar(false)
+                .withActionBarDrawerToggle(true)
+                .build()
     }
 
 }
