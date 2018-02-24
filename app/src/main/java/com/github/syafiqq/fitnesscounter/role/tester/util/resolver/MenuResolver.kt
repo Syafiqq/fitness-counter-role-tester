fc0f1ff2fc901fefe7ae5ba0a2aee736a957cf62
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-02-24 07:18:51 +0700

    Fix Menu Resolver

--- app/src/main/java/com/github/syafiqq/fitnesscounter/role/tester/util/resolver/MenuResolver.kt
+++ app/src/main/java/com/github/syafiqq/fitnesscounter/role/tester/util/resolver/MenuResolver.kt
@@ -1,7 +1,5 @@
 package com.github.syafiqq.fitnesscounter.role.tester.util.resolver
 
-import com.github.syafiqq.fitnesscounter.role.tester.R
-
 /**
  * This fitness-counter-role-tester project created by :
  * Name         : syafiq
@@ -13,13 +11,13 @@ object MenuResolver
 {
     val dashboardDrawerResolver = mutableMapOf(
             // @formatter:off
-            "Medical Check"  to R.id.nav_health,
-            "Ulinois"        to R.id.nav_ulinois,
-            "Vertical Jump"  to R.id.nav_vertical_jump,
-            "Throwing Ball"  to R.id.nav_throwing_ball,
-            "Push Up"        to R.id.nav_push_up,
-            "Sit Up"         to R.id.nav_sit_up,
-            "Run 1600 m"     to R.id.nav_run_1600m
+            "Medical Check"  to 0x1001,
+            "Ulinois"        to 0x1002,
+            "Vertical Jump"  to 0x1003,
+            "Throwing Ball"  to 0x1004,
+            "Push Up"        to 0x1005,
+            "Sit Up"         to 0x1006,
+            "Run 1600 m"     to 0x1007
             // @formatter:on
     )
 }
