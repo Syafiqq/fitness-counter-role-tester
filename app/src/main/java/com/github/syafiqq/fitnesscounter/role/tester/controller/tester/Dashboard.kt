56b43e26b35650df6ccaf538fc848bf6f5f15d50
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-02-24 11:50:03 +0700

    Initialize Event Name

--- app/src/main/java/com/github/syafiqq/fitnesscounter/role/tester/controller/tester/Dashboard.kt
+++ app/src/main/java/com/github/syafiqq/fitnesscounter/role/tester/controller/tester/Dashboard.kt
@@ -2,9 +2,16 @@ package com.github.syafiqq.fitnesscounter.role.tester.controller.tester
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.github.syafiqq.fitnesscounter.core.custom.com.google.firebase.database.CChildEventListener
+import com.github.syafiqq.fitnesscounter.core.custom.com.google.firebase.database.CValueEventListener
+import com.github.syafiqq.fitnesscounter.core.db.external.DataMapper
+import com.github.syafiqq.fitnesscounter.core.db.external.poko.Event
 import com.github.syafiqq.fitnesscounter.role.tester.R
+import com.github.syafiqq.fitnesscounter.role.tester.model.Settings
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.FirebaseDatabase
 import com.mikepenz.materialdrawer.AccountHeader
 import com.mikepenz.materialdrawer.AccountHeaderBuilder
 import com.mikepenz.materialdrawer.Drawer
@@ -48,17 +55,48 @@ class Dashboard: AppCompatActivity()
                 .withOnDrawerItemClickListener({ view, index, menu -> Timber.d("Catch Item [$view, $index, $menu"); false })
                 .build()
 
-
-        this.drawerHeader.addProfile(ProfileDrawerItem().withName("Mike Penz"), 0)
-
         this.user = FirebaseAuth.getInstance().currentUser
 
         Timber.d("Current user [${user?.uid}]")
         Timber.d("Current Selected [${drawerHeader.activeProfile?.name}]")
+
+        this.listRegisteredEvent()
+    }
+
+    private fun listRegisteredEvent()
+    {
+        Timber.d("listRegisteredEvent")
+        FirebaseDatabase.getInstance().getReference(DataMapper.event(user?.uid, Settings.GROUP_NAME)["users"]).addChildEventListener(object:
+                CChildEventListener
+        {
+            override fun onChildAdded(snapshot: DataSnapshot?, previousChildName: String?)
+            {
+                snapshot?.run {
+                    FirebaseDatabase.getInstance().getReference(DataMapper.event(null, null, this.key)["events"]).addListenerForSingleValueEvent(object:
+                            CValueEventListener
+                    {
+                        override fun onDataChange(snapshot: DataSnapshot?)
+                        {
+                            snapshot?.run { this@Dashboard.addNewEvent(this.getValue(Event::class.java)) }
+                        }
+                    })
+                }
+            }
+        })
+    }
+
+    private fun addNewEvent(event: Event?)
+    {
+        Timber.d("addNewEvent [$event]")
+        event?.run {
+            this@Dashboard.drawerHeader.addProfiles(ProfileDrawerItem().withName(this.event))
+        }
     }
 
     override fun onSaveInstanceState(state: Bundle?)
     {
+        Timber.d("onSaveInstanceState [$state]")
+
         var state = state ?: Bundle()
 
         state = drawerHeader.saveInstanceState(state)
@@ -68,6 +106,8 @@ class Dashboard: AppCompatActivity()
 
     override fun onBackPressed()
     {
+        Timber.d("onBackPressed")
+
         if (drawer.isDrawerOpen)
         {
             drawer.closeDrawer()
