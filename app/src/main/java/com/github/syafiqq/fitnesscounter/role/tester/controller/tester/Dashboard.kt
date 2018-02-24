de1257775a9e8cc2d48ddd18a926bb49a93d5762
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-02-24 08:27:19 +0700

    Remove fab

--- app/src/main/java/com/github/syafiqq/fitnesscounter/role/tester/controller/tester/Dashboard.kt
+++ app/src/main/java/com/github/syafiqq/fitnesscounter/role/tester/controller/tester/Dashboard.kt
@@ -1,7 +1,6 @@
 package com.github.syafiqq.fitnesscounter.role.tester.controller.tester
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import com.github.syafiqq.fitnesscounter.role.tester.R
 import com.mikepenz.materialdrawer.Drawer
@@ -10,7 +9,6 @@ import kotlinx.android.synthetic.main.tester_activity_dashboard.*
 
 class Dashboard: AppCompatActivity()
 {
-
     private lateinit var drawer: Drawer
 
     override fun onCreate(savedInstanceState: Bundle?)
@@ -18,11 +16,6 @@ class Dashboard: AppCompatActivity()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.tester_activity_dashboard)
         setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
 
         this.drawer = DrawerBuilder()
                 .withActivity(this)
