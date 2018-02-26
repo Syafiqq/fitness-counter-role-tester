e65af7782b35289588c14bb38a50ccdc3ff46b11
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-02-26 21:31:54 +0700

    Add stopwatchService Initialization

--- app/src/main/java/com/github/syafiqq/fitnesscounter/role/tester/App.kt
+++ app/src/main/java/com/github/syafiqq/fitnesscounter/role/tester/App.kt
@@ -3,8 +3,13 @@ package com.github.syafiqq.fitnesscounter.role.tester
 import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Build
+import android.os.IBinder
+import com.github.syafiqq.fitnesscounter.role.tester.controller.service.StopwatchService
 import org.acra.ACRA
 import org.acra.ReportField
 import org.acra.ReportingInteractionMode
@@ -25,6 +30,8 @@ import timber.log.Timber
         resToastText = R.string.crash_toast_text)
 class App: Application()
 {
+    val stopwatchService = StopwatchService.Observable()
+
     override fun onCreate()
     {
         Timber.d("onCreate")
@@ -39,6 +46,8 @@ class App: Application()
                 this?.createNotificationChannel(NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_LOW))
             }
         }
+        App.instance = this
+        this.bindStopwatchService()
     }
 
     override fun attachBaseContext(base: Context)
@@ -61,5 +70,34 @@ class App: Application()
         )
 
         Timber.d("Timber initialized")
+    }
+
+    private fun bindStopwatchService()
+    {
+        Timber.d("bindStopwatchService")
+
+        super.bindService(Intent(this, StopwatchService::class.java), stopwatchServiceConnection, Context.BIND_AUTO_CREATE)
+    }
+
+    private val stopwatchServiceConnection = object: ServiceConnection
+    {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?)
+        {
+            Timber.d("onServiceConnected [$name, $service]")
+
+            stopwatchService.service = (service as StopwatchService.StopwatchServiceBinder).service
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?)
+        {
+            Timber.d("onServiceConnected [$name]")
+
+            stopwatchService.service = null
+        }
+    }
+
+    companion object
+    {
+        lateinit var instance: App
     }
 }
