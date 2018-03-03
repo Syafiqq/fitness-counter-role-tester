7452aedf33b66c38391dd664d124ce927b1c6e48
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-03-03 20:53:17 +0700

    Move common function to parent function

--- app/src/main/java/com/github/syafiqq/fitnesscounter/role/tester/controller/tester/fragment/IdentifiableFragment.kt
+++ app/src/main/java/com/github/syafiqq/fitnesscounter/role/tester/controller/tester/fragment/IdentifiableFragment.kt
@@ -1,6 +1,12 @@
 package com.github.syafiqq.fitnesscounter.role.tester.controller.tester.fragment
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.View
+import com.afollestad.materialdialogs.MaterialDialog
+import com.github.syafiqq.fitnesscounter.role.tester.R
+import timber.log.Timber
+import java.util.concurrent.atomic.AtomicReference
 
 /**
  * This fitness-counter-role-tester project created by :
@@ -10,6 +16,29 @@ import android.support.v4.app.Fragment
  * Github       : Syafiqq
  */
 abstract class IdentifiableFragment: Fragment()
-{
+{ // @formatter:off
     abstract val identifier: String
-}
+    protected var dialog = AtomicReference<MaterialDialog>(null)
+    protected val dialogs = mutableMapOf<String, MaterialDialog>()
+    protected open fun doSend(v: View? = null){}
+    protected open fun saveChanges(){}
+    protected open fun loadChanges(){}
+
+    override fun onViewCreated(view: View, state: Bundle?)
+    {
+        Timber.d("onViewCreated [$view, $state]")
+        this.dialogs.putAll(mapOf(
+                "please-wait" to MaterialDialog.Builder(this.context!!)
+                        .canceledOnTouchOutside(false)
+                        .content(super.getResources().getString(R.string.label_please_wait))
+                        .progress(true, 0)
+                        .build(),
+                "confirmation-send" to MaterialDialog.Builder(this.context!!)
+                        .title("Konfirmasi")
+                        .positiveText("Ya")
+                        .negativeText("Tidak")
+                        .onPositive { _, _ -> doSend() }
+                        .build()))
+        super.onViewCreated(view, state)
+    }
+} // @formatter:on
