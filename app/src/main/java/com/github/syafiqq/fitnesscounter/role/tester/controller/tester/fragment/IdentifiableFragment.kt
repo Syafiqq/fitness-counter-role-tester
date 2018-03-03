fa19b658c260eaf1e03a42f27a810667bc3a0a8f
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-03-03 15:57:30 +0700

    Add Identifiable Fragment

---
+++ app/src/main/java/com/github/syafiqq/fitnesscounter/role/tester/controller/tester/fragment/IdentifiableFragment.kt
@@ -0,0 +1,15 @@
+package com.github.syafiqq.fitnesscounter.role.tester.controller.tester.fragment
+
+import android.support.v4.app.Fragment
+
+/**
+ * This fitness-counter-role-tester project created by :
+ * Name         : syafiq
+ * Date / Time  : 03 March 2018, 3:49 PM.
+ * Email        : id.muhammad.syafiq@gmail.com
+ * Github       : Syafiqq
+ */
+abstract class IdentifiableFragment: Fragment()
+{
+    abstract val identifier: String
+}
