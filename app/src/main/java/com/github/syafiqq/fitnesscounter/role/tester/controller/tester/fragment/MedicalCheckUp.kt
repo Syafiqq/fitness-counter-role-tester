1cb8e78f8e0b66f06962e6c76d13f6d7227e74c3
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-03-03 19:23:18 +0700

    Fix Medical checkup interaction flow

--- app/src/main/java/com/github/syafiqq/fitnesscounter/role/tester/controller/tester/fragment/MedicalCheckUp.kt
+++ app/src/main/java/com/github/syafiqq/fitnesscounter/role/tester/controller/tester/fragment/MedicalCheckUp.kt
@@ -93,20 +93,6 @@ class MedicalCheckUp: IdentifiableFragment()
         super.onViewCreated(view, state)
     }
 
-    private fun calculateRatio(tbb: Float?, tbd: Float?): Float?
-    {
-        val ratio = if (tbb == null || tbd == null) null else ((tbb - tbd) / tbd).takeIf { it.isFinite() }
-        this.h_edittext_ratio.setText(String.format(Locale.getDefault(), "%.3f", ratio ?: 0f))
-        return ratio
-    }
-
-    private fun calculateBmi(tbb: Float?, weight: Float?): Float?
-    {
-        val bmi = if (tbb == null || weight == null) null else (weight / (tbb / 100).pow(2)).takeIf { it.isFinite() }
-        this.h_edittext_bmi.setText(String.format(Locale.getDefault(), "%.3f", bmi ?: 0f))
-        return bmi
-    }
-
     override fun onAttach(context: Context)
     {
         Timber.d("onAttach [$context]")
@@ -120,6 +106,43 @@ class MedicalCheckUp: IdentifiableFragment()
         {
             throw RuntimeException(context.toString() + " must implement OnInteractionListener")
         }
+    }
+
+    override fun onSaveInstanceState(state: Bundle)
+    {
+        Timber.d("onSaveInstanceState [$state]")
+
+
+        state.putSerializable(M_RESULT, this.checkUp)
+        state.putInt(M_PARTICIPANT, this.h_edittext_participant.text.toString().toIntOrNull() ?: 0)
+        super.onSaveInstanceState(state)
+    }
+
+    override fun onActivityCreated(state: Bundle?)
+    {
+        Timber.d("onActivityCreated [$state]")
+
+        super.onActivityCreated(state)
+        state?.let {
+            it.getSerializable(M_RESULT)?.let { this.checkUp.set(it as MedicalCheckup) }
+            it.getInt(M_PARTICIPANT).let { this.h_edittext_participant.setText(if (it == 0) "" else it.toString()) }
+        }
+
+        this.loadChanges()
+    }
+
+    private fun calculateRatio(tbb: Float?, tbd: Float?): Float?
+    {
+        val ratio = if (tbb == null || tbd == null) null else ((tbb - tbd) / tbd).takeIf { it.isFinite() }
+        this.h_edittext_ratio.setText(String.format(Locale.getDefault(), "%.3f", ratio ?: 0f))
+        return ratio
+    }
+
+    private fun calculateBmi(tbb: Float?, weight: Float?): Float?
+    {
+        val bmi = if (tbb == null || weight == null) null else (weight / (tbb / 100).pow(2)).takeIf { it.isFinite() }
+        this.h_edittext_bmi.setText(String.format(Locale.getDefault(), "%.3f", bmi ?: 0f))
+        return bmi
     }
 
     fun doSend(v: View? = null)
@@ -187,6 +210,99 @@ class MedicalCheckUp: IdentifiableFragment()
         this.checkUp.conclusion = this.radiogroup_kesimpulan.checkedRadioButtonId == this.radiobutton_kesimpulan_yes.id
     }
 
+    private fun loadChanges()
+    {
+        Timber.d("loadChanges")
+
+        // Anthropometric
+        this.h_edittext_tbb.setText(if (this.checkUp.tbb == null) "" else String.format(Locale.getDefault(), "%.2f", this.checkUp.tbb))
+        this.h_edittext_tbd.setText(if (this.checkUp.tbd == null) "" else String.format(Locale.getDefault(), "%.2f", this.checkUp.tbd))
+        this.h_edittext_weight.setText(if (this.checkUp.weight == null) "" else String.format(Locale.getDefault(), "%.2f", this.checkUp.weight))
+        // Posture and Gait
+        when (this.checkUp.posture)
+        {
+            this.radiobutton_postur_normal.text    -> this.radiobutton_postur_normal.isSelected = true
+            this.radiobutton_postur_kifosis.text   -> this.radiobutton_postur_kifosis.isSelected = true
+            this.radiobutton_postur_skoliosis.text -> this.radiobutton_postur_skoliosis.isSelected = true
+            this.radiobutton_postur_ordosis.text   -> this.radiobutton_postur_ordosis.isSelected = true
+        }
+        when (this.checkUp.gait)
+        {
+            this.radiobutton_aa_deformitas.text -> this.radiobutton_aa_deformitas.isSelected = true
+            this.radiobutton_aa_kelainan.text   -> this.radiobutton_aa_kelainan.isSelected = true
+            this.radiobutton_aa_kelemahan.text  -> this.radiobutton_aa_kelemahan.isSelected = true
+            this.radiobutton_aa_normal.text     -> this.radiobutton_aa_normal.isSelected = true
+        }
+        // Cardiovascular
+        this.h_edittext_denyut.setText(if (this.checkUp.pulse == null) "" else String.format(Locale.getDefault(), "%.2f", this.checkUp.pulse))
+        if (this.checkUp.pressure == null)
+        {
+            this.h_edittext_mm.setText("")
+            this.h_edittext_hg.setText("")
+        }
+        else
+        {
+            val mmHGSplitter = this.checkUp.pressure!!.split(" / ")
+            val mm = mmHGSplitter[0].toFloatOrNull()
+            val hg = mmHGSplitter[1].toFloatOrNull()
+            this.h_edittext_mm.setText(if (mm == null) "" else String.format(Locale.getDefault(), "%.2f", mm))
+            this.h_edittext_hg.setText(if (hg == null) "" else String.format(Locale.getDefault(), "%.2f", hg))
+        }
+        when (this.checkUp.ictus)
+        {
+            this.radiobutton_ictus_minus.text -> this.radiobutton_ictus_minus.isSelected = true
+            this.radiobutton_ictus_plus.text  -> this.radiobutton_ictus_plus.isSelected = true
+        }
+        when (this.checkUp.heart)
+        {
+            this.radiobutton_jantung_normal.text -> this.radiobutton_jantung_normal.isSelected = true
+            this.radiobutton_jantung_tidak.text  -> this.radiobutton_jantung_tidak.isSelected = true
+        }
+        // Respiratory
+        this.h_edittext_frekuensi.setText(if (this.checkUp.frequency == null) "" else String.format(Locale.getDefault(), "%.2f", this.checkUp.frequency))
+        when (this.checkUp.retraction)
+        {
+            this.radiobutton_retraksi_minus.text -> this.radiobutton_retraksi_minus.isSelected = true
+            this.radiobutton_retraksi_plus.text  -> this.radiobutton_retraksi_plus.isSelected = true
+        }
+        this.h_edittext_lokasi_retraksi.setText(this.checkUp.rLocation ?: "")
+        when (this.checkUp.breath)
+        {
+            this.radiobutton_suara_napas_abnormal.text -> this.radiobutton_suara_napas_abnormal.isSelected = true
+            this.radiobutton_suara_napas_normal.text   -> this.radiobutton_suara_napas_normal.isSelected = true
+        }
+        when (this.checkUp.bPipeline)
+        {
+            this.radiobutton_saluran_napas_normal.text    -> this.radiobutton_saluran_napas_normal.isSelected = true
+            this.radiobutton_saluran_napas_obstruksi.text -> this.radiobutton_saluran_napas_obstruksi.isSelected = true
+        }
+        // Verbal
+        when (this.checkUp.vision)
+        {
+            this.radiobutton_mata_julig.text  -> this.radiobutton_mata_julig.isSelected = true
+            this.radiobutton_mata_normal.text -> this.radiobutton_mata_normal.isSelected = true
+            this.radiobutton_mata_pms.text    -> this.radiobutton_mata_pms.isSelected = true
+        }
+        when (this.checkUp.hearing)
+        {
+            this.radiobutton_telinga_normal.text     -> this.radiobutton_telinga_normal.isSelected = true
+            this.radiobutton_telinga_tuli.text       -> this.radiobutton_telinga_tuli.isSelected = true
+            this.radiobutton_telinga_obstruktif.text -> this.radiobutton_telinga_obstruktif.isSelected = true
+        }
+        when (this.checkUp.verbal)
+        {
+            this.radiobutton_verbal_latah.text  -> this.radiobutton_verbal_latah.isSelected = true
+            this.radiobutton_verbal_normal.text -> this.radiobutton_verbal_normal.isSelected = true
+            this.radiobutton_verbal_tuna.text   -> this.radiobutton_verbal_tuna.isSelected = true
+        }
+        // Conclusion
+        when (this.checkUp.conclusion)
+        {
+            false -> this.radiobutton_kesimpulan_no.isSelected = true
+            true  -> this.radiobutton_kesimpulan_yes.isSelected = true
+        }
+    }
+
     interface OnInteractionListener
     {
         fun getEvent(): Event
@@ -200,6 +316,8 @@ class MedicalCheckUp: IdentifiableFragment()
         }
 
         const val IDENTIFIER = "Medical Check"
+        const val M_RESULT = "m_result"
+        const val M_PARTICIPANT = "m_participant"
     }
 
     fun Editable.toFloat(): Float?
