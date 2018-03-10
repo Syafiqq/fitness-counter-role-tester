2f944fc516402a7f2d5931e4ea6474521c2bd208
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-03-10 11:07:18 +0700

    Initial Finishing for Run1600m

--- app/src/main/java/com/github/syafiqq/fitnesscounter/role/tester/controller/tester/fragment/Run1600m.kt
+++ app/src/main/java/com/github/syafiqq/fitnesscounter/role/tester/controller/tester/fragment/Run1600m.kt
@@ -6,26 +6,95 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.danielbostwick.stopwatch.core.model.Stopwatch
 import com.github.syafiqq.fitnesscounter.core.db.external.poko.Event
+import com.github.syafiqq.fitnesscounter.core.helpers.tester.PresetHelper
 import com.github.syafiqq.fitnesscounter.role.tester.R
+import com.github.syafiqq.fitnesscounter.role.tester.controller.service.StopwatchService
+import com.github.syafiqq.fitnesscounter.role.tester.ext.com.afollestad.materialdialogs.changeAndShow
+import com.github.syafiqq.fitnesscounter.role.tester.ext.com.afollestad.materialdialogs.org.joda.time.toFormattedStopwatch
+import com.google.firebase.database.DatabaseReference
+import kotlinx.android.synthetic.main.fragment_tester_run1600m.*
+import org.joda.time.DateTime
+import org.joda.time.Duration
 import timber.log.Timber
+import java.util.Observer
+import java.util.Timer
+import java.util.TimerTask
+import kotlin.properties.Delegates
+import com.github.syafiqq.fitnesscounter.core.db.external.poko.tester.Run1600m as MRun1600m
 
 class Run1600m: IdentifiableFragment()
 {
     override val identifier: String
         get() = Run1600m.IDENTIFIER
     private lateinit var listener: OnInteractionListener
+    private var timer: Timer? = null
+
+    private var updateText = createTimerTask()
+    private var stopwatchService: StopwatchService? = null
+    private val stopwatchO = Observer { o, arg ->
+        if (o is StopwatchService.Observable)
+        {
+            Timber.d("Stopwatch Initialized")
+            stopwatchService = arg as StopwatchService
+        }
+    }
+    private var stopwatchState by Delegates.observable(StopwatchStatus.PREPARED) { _, _, new -> shiftUI(new) }
+
+    private val run = MRun1600m()
+
+    override fun onCreate(state: Bundle?)
+    {
+        Timber.d("onCreate [$state]")
+        super.onCreate(state)
+
+        with(this.listener.getOService())
+        {
+            this.addObserver(stopwatchO)
+            stopwatchService = this.service
+        }
+    }
+
+    override fun onDestroy()
+    {
+        Timber.d("onDestroy")
+        this.listener.getOService().deleteObserver(stopwatchO)
+        super.onDestroy()
+    }
+
+    override fun onResume()
+    {
+        Timber.d("onResume")
+
+        super.onResume()
+        this.watchStopwatch()
+    }
+
+    override fun onPause()
+    {
+        Timber.d("onPause")
+
+        this.unwatchStopwatch()
+        super.onPause()
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, state: Bundle?): View?
     {
         Timber.d("onCreateView [$inflater, $container, $state]")
-        return inflater.inflate(R.layout.fragment_tester_run1600m, container, false)
+        return inflater.inflate(R.layout.fragment_tester_run, container, false)
     }
 
     override fun onViewCreated(view: View, state: Bundle?)
     {
         Timber.d("onViewCreated [$view, $state]")
 
+        this.button_send.setOnClickListener { _ -> this.dialog.changeAndShow(this.dialogs["confirmation-send"].apply { this?.setContent("Apakah anda yakin mengirim nilai peserta ${this@Run1600m.edittext_participant.text}") }!!) }
+
+        this.button_start.setOnClickListener(this::startStopwatch)
+        this.button_stop.setOnClickListener(this::stopStopwatch)
+        this.button_reset.setOnClickListener(this::resetStopwatch)
         super.onViewCreated(view, state)
     }
 
@@ -44,9 +113,217 @@ class Run1600m: IdentifiableFragment()
         }
     }
 
+    override fun onSaveInstanceState(state: Bundle)
+    {
+        Timber.d("onSaveInstanceState [$state]")
+
+        state.putSerializable(M_RESULT, this.run)
+        state.putInt(M_PARTICIPANT, this.edittext_participant.text.toString().toIntOrNull() ?: 0)
+        state.putString(M_STOPWATCH_STATE, this.stopwatchState.name)
+        this.stopwatchService?.let { state.putSerializable(M_STOPWATCH, it.getStopwatch()) }
+        super.onSaveInstanceState(state)
+    }
+
+    override fun onActivityCreated(state: Bundle?)
+    {
+        Timber.d("onActivityCreated [$state]")
+
+        super.onActivityCreated(state)
+        state?.let {
+            it.getSerializable(M_RESULT)?.let { this.run.set(it as MRun1600m) }
+            it.getInt(M_PARTICIPANT).let { this.edittext_participant.setText(if (it == 0) "" else it.toString()) }
+            it.getString(M_STOPWATCH_STATE)?.let { this.stopwatchState = StopwatchStatus.valueOf(it) }
+            if (this.stopwatchService != null) it.getSerializable(M_STOPWATCH)?.let { this.stopwatchService!!.getStopwatch().set(it as Stopwatch) }
+        }
+
+        this.loadChanges()
+    }
+
+    override fun doSend(v: View?)
+    {
+        Timber.d("doSend [$v]")
+
+        this.saveChanges()
+        val event = this.listener.getEvent()
+        if (event.presetActive != null)
+        {
+            if (this.edittext_participant.text.toString().toIntOrNull() == null)
+            {
+                Toast.makeText(this.context!!, "Nomor Peserta Tidak Valid", Toast.LENGTH_LONG).show()
+            }
+            else
+            {
+                this.dialog.changeAndShow(this.dialogs["please-wait"]!!)
+                PresetHelper.saveRun1600m(event.presetActive!!, this.edittext_participant.text.toString().toInt(), this.run, DatabaseReference.CompletionListener { error, _ ->
+                    run {
+                        with(this@Run1600m)
+                        {
+                            if (error == null)
+                            {
+                                Toast.makeText(this.context!!, "Pengiriman Berhasil", Toast.LENGTH_LONG).show()
+                            }
+                            else
+                            {
+                                Toast.makeText(this.context!!, "Error Pengiriman, Lebih baik simpan terlebih dahulu", Toast.LENGTH_LONG).show()
+                            }
+                            this.dialog.get()?.dismiss()
+                        }
+                    }
+                })
+            }
+        }
+    }
+
+    override fun saveChanges()
+    {
+        Timber.d("saveChanges")
+    }
+
+    override fun loadChanges()
+    {
+        Timber.d("loadChanges")
+        this.run.start?.let { if (this.stopwatchService != null) this.stopwatchService?.getStopwatch()?.startedAt = DateTime(it) }
+        this.run.elapsed?.let { this.displayStopwatch(Duration.millis(if (stopwatchState == StopwatchStatus.PREPARED) 0L else it)) }
+    }
+
     interface OnInteractionListener
     {
         fun getEvent(): Event
+        fun getOService(): StopwatchService.Observable
+    }
+
+    private fun startStopwatch(view: View? = null)
+    {
+        Timber.d("startStopwatch [$view]")
+
+        this.stopwatchService?.run {
+            this.reset(this.getStopwatch())
+            this.start(this.getStopwatch(), DateTime.now())
+            with(this@Run1600m)
+            {
+                this.run.start = this@run.getStopwatch().startedAt.millis
+                this.stopwatchState = StopwatchStatus.STARTED
+            }
+        }
+    }
+
+    private fun stopStopwatch(view: View? = null)
+    {
+        Timber.d("stopStopwatch [$view]")
+
+        this.stopwatchService?.run {
+            this.pause(this.getStopwatch(), DateTime.now())
+            with(this@Run1600m)
+            {
+                this.run.end = this@run.getStopwatch().startedAt.millis
+                this.run.elapsed = (this.run.end ?: 0L) - (this.run.start ?: 0L)
+                this.stopwatchState = StopwatchStatus.STOPPED
+                this.run.elapsed?.let { this.displayStopwatch(org.joda.time.Duration.millis(it)) }
+            }
+        }
+    }
+
+    private fun resetStopwatch(view: View? = null)
+    {
+        Timber.d("resetStopwatch [$view]")
+
+        this.stopwatchService?.run {
+            this.reset(this.getStopwatch())
+            with(this@Run1600m)
+            {
+                this.stopwatchState = StopwatchStatus.PREPARED
+                this.displayStopwatch(Duration.millis(0L))
+            }
+        }
+    }
+
+    private fun shiftUI(state: StopwatchStatus)
+    {
+        when (state)
+        {
+            StopwatchStatus.PREPARED ->
+            {
+                this.button_start.visibility = View.VISIBLE
+                this.button_stop.visibility = View.GONE
+                this.group_finish.visibility = View.GONE
+                this.unwatchStopwatch()
+            }
+            StopwatchStatus.STARTED  ->
+            {
+                this.button_start.visibility = View.GONE
+                this.button_stop.visibility = View.VISIBLE
+                this.group_finish.visibility = View.GONE
+                this.watchStopwatch()
+            }
+            StopwatchStatus.STOPPED  ->
+            {
+                this.button_start.visibility = View.GONE
+                this.button_stop.visibility = View.GONE
+                this.group_finish.visibility = View.VISIBLE
+                this.unwatchStopwatch()
+            }
+        }
+    }
+
+    private fun createTimerTask(): TimerTask
+    {
+        Timber.d("createTimerTask")
+
+        return object: TimerTask()
+        {
+            override fun run()
+            {
+                with(this@Run1600m)
+                {
+                    this.activity?.runOnUiThread {
+                        this.stopwatchService?.let {
+                            this.displayStopwatch(it.timeElapsed(it.getStopwatch(), DateTime.now()))
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun watchStopwatch()
+    {
+        Timber.d("watchStopwatch")
+
+        if (stopwatchState == StopwatchStatus.STARTED)
+        {
+            this.unwatchStopwatch()
+            timer = Timer().apply {
+                try
+                {
+                    this.scheduleAtFixedRate(updateText, 0, TIMER_DELAY)
+                }
+                catch (e: Exception)
+                {
+                    updateText = createTimerTask()
+                    this.scheduleAtFixedRate(updateText, 0, TIMER_DELAY)
+                    Timber.e(e)
+                }
+            }
+        }
+    }
+
+    private fun unwatchStopwatch()
+    {
+        Timber.d("unwatchStopwatch")
+
+        timer?.cancel()
+    }
+
+    private fun displayStopwatch(duration: Duration)
+    {
+        this.textview_clock.text = duration.toFormattedStopwatch()
+    }
+
+    private enum class StopwatchStatus
+    {
+        PREPARED,
+        STARTED,
+        STOPPED
     }
 
     companion object
@@ -56,6 +333,13 @@ class Run1600m: IdentifiableFragment()
             return Run1600m()
         }
 
-        const val IDENTIFIER = "Run 1600 m"
+        const val IDENTIFIER = "Run1600m"
+        const val M_RESULT = "m_result"
+        const val M_PARTICIPANT = "m_participant"
+        const val M_STOPWATCH_STATE = "m_stopwatch_state"
+        const val M_STOPWATCH = "m_stopwatch"
+        const val TIMER_DELAY: Long = 1000
     }
 }
+
+class IdRun1600m(id: Int? = null, run: MRun1600m = MRun1600m())
